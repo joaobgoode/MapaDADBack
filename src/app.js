2840ab1aeb142ea8a8ee80a66284dbb149e9865f
João Victor <joaovictorvvm@edu.unifor.br>
2025-05-06 12:22:41 -0300

    added http logging (morgan)

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 require('dotenv').config();
 require('./config/database');
 const cors = require('cors');
+const morgan = require('morgan');
 
 module.exports = function (wss) {
   const app = express();
   app.use(cors());
   app.use(express.json());
+  app.use(morgan('dev'));
 
   app.use('/api/user', require('./routes/user.routes'));
   app.use('/api/horarios', require('./routes/horario.routes')(wss));
