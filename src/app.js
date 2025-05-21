38ef0f83ae8f36f9e01feb6da5b1ea4df65b553c
João Victor <joaovictorvvm@edu.unifor.br>
2025-05-20 23:20:01 -0300

    removed csrf

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 require('dotenv').config();
 require('./config/database');
-
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -16,7 +15,7 @@ module.exports = function (wss) {
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token']
+    allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
   app.use(helmet());
@@ -34,45 +33,21 @@ module.exports = function (wss) {
     }
   }));
 
-  app.use(lusca.csrf());
   app.use(lusca.xframe('SAMEORIGIN'));
   app.use(lusca.xssProtection(true));
-
   app.use(morgan("dev"));
 
   app.use((req, res, next) => {
     console.log('Sessão ID:', req.sessionID);
-    console.log('Token CSRF recebido:', req.headers['x-csrf-token']);
     next();
   });
 
-  app.get('/api/csrf-token', (req, res) => {
-    if (!req.session) {
-      return res.status(500).json({ error: 'Sessão não iniciada' });
-    }
-
-    try {
-      const csrfToken = req.csrfToken();
-      res.json({ csrfToken });
-    } catch (err) {
-      console.error('Erro ao gerar token CSRF:', err);
-      res.status(500).json({ error: 'Erro ao gerar token CSRF' });
-    }
-  });
 
   app.use('/login', require('./routes/auth.routes'));
   app.use('/api/users', require('./routes/user.routes')(wss));
   app.use('/api/spaces', require('./routes/spaces.routes')(wss));
   app.use('/api/horarios', require('./routes/horario.routes')(wss));
 
-  app.use((err, req, res, next) => {
-    if (err.code === 'EBADCSRFTOKEN') {
-      return res.status(403).json({
-        message: 'Formulário expirado ou inválido. Por favor, tente novamente.'
-      });
-    }
-    next(err);
-  });
 
   return app;
 };
